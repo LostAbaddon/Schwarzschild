fc07022337d1c018d34db3b4c4b4bc85dedbfb0d
LostAbaddon <lostabaddon@foxmail.com>
2021-02-08 16:00:07 +0800

    调整初始页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<HelloWorld title="Welcome to LostAbaddon's Death Foreseer Society!" msg="The world is waiting for its light, please WAIT for a while..."/>
+	</div>
 </template>
 
 <script>
@@ -10,9 +9,9 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+	components: {
+		HelloWorld
+	}
 }
 </script>
