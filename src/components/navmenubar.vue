ec918b52f9966d63d0f0a7633f746952836f5692
LostAbaddon <lostabaddon@foxmail.com>
2021-02-10 00:28:43 +0800

    顶部导航条

---
+++ src/components/navmenubar.vue
@@ -0,0 +1,14 @@
+<template>
+	<nav class="nav-menu-bar">
+		<NavMenuItem v-for="item in menu" :title="item[0]" :action="item[1]" :subs="item[2]" />
+	</nav>
+</template>
+
+<script>
+export default {
+	name: 'NavMenuBar',
+	props: {
+		menu: Array
+	}
+}
+</script>
---
+++ src/components/navmenuitem.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="nav-menu-item">
+		<router-link :to="action">{{title}}<font-awesome-icon icon="caret-down" class="nav-fa" v-if="!!subs" /></router-link>
+		<NavMenuBar v-if="!!subs" :menu="subs" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NavMenuItem',
+	props: {
+		title: String,
+		action: String,
+		subs: Array
+	}
+}
+</script>
