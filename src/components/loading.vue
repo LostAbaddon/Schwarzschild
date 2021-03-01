0d5985d9015346e6783e85bedc43c317292b604c
LostAbaddon <lostabaddon@foxmail.com>
2021-03-01 17:13:13 +0800

    增加前置关闭导入蒙层的功能

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -11,6 +11,7 @@
 			</div>
 			<div class="title">{{title}}</div>
 		</div>
+		<div class="forceQuit" v-if="forcequit" @click="forceQuit">强制关闭</div>
 	</div>
 </template>
 
@@ -20,6 +21,7 @@ global.callPageLoaded = () => {
 	var ch = new BroadcastChannel('change-loading-hint');
 	ch.postMessage({action: 'hide'});
 };
+var quiter = null;
 
 export default {
 	name: "Loading",
@@ -27,23 +29,46 @@ export default {
 		return {
 			title: "载入中……",
 			show: true,
-			type: "pulse"
+			type: "pulse",
+			forcequit: false
 		}
 	},
 	created () {
-		channel.addEventListener('message', async msg => {
+		channel.addEventListener('message', msg => {
 			var data = msg.data;
 			if (data.action === 'show') {
 				data.title = data.title || '载入中……';
+				this.forcequit = false;
+				if (!!quiter) {
+					clearTimeout(quiter);
+					quiter = null;
+				}
+				quiter = setTimeout(() => {
+					this.forcequit = true;
+				}, 5000);
 				this.show = true;
 			}
 			else if (data.action === 'hide') {
 				data.title = data.title || '载入中……';
+				this.forcequit = false;
+				if (!!quiter) {
+					clearTimeout(quiter);
+					quiter = null;
+				}
 				this.show = false;
 			}
 			if (String.is(data.title)) this.title = data.title;
 			if (['spin', 'pulse'].indexOf(data.type) >= 0) this.type = data.type;
 		});
+	},
+	methods: {
+		forceQuit () {
+			if (!!quiter) {
+				clearTimeout(quiter);
+				quiter = null;
+			}
+			this.show = false;
+		}
 	}
 }
 </script>
