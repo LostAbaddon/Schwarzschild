19c34abec9f704f513294262b46f349e84c129c1
LostAbaddon <lostabaddon@foxmail.com>
2021-03-22 10:53:33 +0800

    优化引用功能

--- public/js/worker/extmarkup.js
+++ public/js/worker/extmarkup.js
@@ -22,12 +22,23 @@ MarkUp.addExtension({
 MarkUp.addExtension({
 	name: 'Cite&Repost',
 	parse: (line, doc, caches) => {
-		var match = line.match(/^[ 　\t]*CITE:[ 　\t]*(.*?)[ 　\t]*$/);
+		var changed = false;
+		line = line.replace(/<\[(\d+)\]>/g, (match, index) => {
+			var key = 'CITE-' + MarkUp.generateRandomKey();
+			caches[key] = '<a href="#CITE-' + index + '">[' + index + ']</a>';
+			changed = true;
+			return '%' + key + '%';
+		});
+
+		var match = line.match(/^[ 　\t]*CITE-(\d+):[ 　\t]*(.*?)[ 　\t]*$/);
 		if (!!match) {
 			doc.metas.others = doc.metas.others || {};
 			doc.metas.others.Cites = doc.metas.others.Cites || [];
-			doc.metas.others.Cites.push(match[1]);
-			return ["", true];
+			var index = match[1] * 1;
+			if (!isNaN(index)) {
+				doc.metas.others.Cites[index] = match[2];
+				return ["", true];
+			}
 		}
 		match = line.match(/^[ 　\t]*REPOST:[ 　\t]*(.*?)[ 　\t]*$/);
 		if (!!match) {
@@ -36,7 +47,7 @@ MarkUp.addExtension({
 			doc.metas.others.Reposts.push(match[1]);
 			return ["", true];
 		}
-		return [line, false];
+		return [line, changed];
 	},
 }, 0, -1);
 MarkUp.addExtension({
@@ -46,11 +57,12 @@ MarkUp.addExtension({
 		if (!!doc.metas.others) {
 			if (!!doc.metas.others.Cites && doc.metas.others.Cites.length > 0) {
 				hasCites = true;
-				content.push('<section><h1><a name="CITE-LIST">引用文献</a></h1><ul>');
-				doc.metas.others.Cites.forEach(cite => {
-					content.push('<li><a href="' + cite + '" target="_blank">' + cite + '</a></li>');
+				content.push('<section><h1><a name="CITE-LIST">引用文献</a></h1>');
+				doc.metas.others.Cites.forEach((cite, index) => {
+					if (!cite) return;
+					content.push('<p><a name="CITE-' + index + '">[' + index + ']</a>：' + MarkUp.parseLine(cite, doc) + '</p>');
 				});
-				content.push("</ul></section>");
+				content.push("</section>");
 			}
 			if (!!doc.metas.others.Reposts && doc.metas.others.Reposts.length > 0) {
 				hasReposts = true;
@@ -63,6 +75,7 @@ MarkUp.addExtension({
 		}
 		text = text.replace(/<\/article>[ 　\t\n\r]*$/, content.join('') + "</article>");
 		if (content.length > 0) {
+			// 添加尾部引用与转载列表
 			content = [];
 			if (hasCites) {
 				content.push('<p class="content-item level-1"><span class="content-indent"></span><a class="content-link" href="#CITE-LIST">引用文献</a></p>');
@@ -70,6 +83,7 @@ MarkUp.addExtension({
 			if (hasReposts) {
 				content.push('<p class="content-item level-1"><span class="content-indent"></span><a class="content-link" href="#REPOST-LIST">转载平台</a></p>');
 			}
+			// 更新目录
 			text = text.replace(/(<aside class="content-table">)([\w\W]*?)(<\/aside>)/g, (match, pre, list, post) => {
 				return pre + list + content.join('') + post;
 			});
