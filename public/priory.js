c930b1f69df7f8eb4ea82ce61b91095cf4b95e0a
LostAbaddon <lostabaddon@foxmail.com>
2021-10-19 16:05:50 +0800

    调整缓存策略

--- public/priory.js
+++ public/priory.js
@@ -7,6 +7,21 @@ const CacheUrl = [
 	'/webapp.json',
 ];
 
+const prepare = async () => {
+	var keys = await caches.keys();
+	if (keys.length > 0) {
+		keys.forEach(async key => {
+			var cacheStorage = await caches.open(key);
+			var cacheKeys = await cacheStorage.keys();
+			cacheKeys.forEach(req => {
+				var url = req.url;
+				if (!CacheUrl.includes(url)) CacheUrl.push(url);
+			});
+		});
+	}
+};
+// prepare();
+
 const cacheResource = async (req, res) => {
 	var cache = await caches.open(CacheName);
 	await cache.put(req, res);
@@ -31,9 +46,13 @@ self.addEventListener('fetch', evt => {
 	pathname = pathname.join('/');
 	if (filename === 'priory.js') return;
 	if (filename.match(/(mp3|mp4|wav|avi|rm|rmvb)$/i)) return;
+	if (filename.match(/hot-update\.json/i)) return;
 	if (!!pathname.match(/^[\/\\]*api[\/\\]/i) || !!pathname.match(/^[\/\\]*api$/i)) return;
 	// if (!fullpath.match(/^\/*#\/+|^\/*#$/)) caches.open(CacheName).then(cache => cache.add(fullpath)); // 将适合的请求都缓存起来
-	caches.open(CacheName).then(cache => cache.add(fullpath)); // 将适合的请求都缓存起来
+	if (!CacheUrl.includes(fullpath)) caches.open(CacheName).then(cache => {
+		cache.add(fullpath);
+		CacheUrl.push(fullpath);
+	}); // 将适合的请求都缓存起来
 	evt.respondWith(caches.match(evt.request).then(cache => {
 		if (cache) {
 			return cache;
