ff999497d8c798a19123c798b51aa9f1f0cd298d
LostAbaddon <lostabaddon@foxmail.com>
2022-12-17 01:17:57 +0800

    增加Asimov线程在网站框架更新后自动重启的功能

--- src/assets/js/markup.js
+++ src/assets/js/markup.js
@@ -4,27 +4,31 @@ window.InitAsimov = async () => {
 		let TaskPool = new Map();
 		let generateID = () => Math.floor(Math.random() * 100000000);
 
-		let asimovWorker, workerPort, workerName;
-		if (!!window.SharedWorker) {
-			workerName = 'Shared-Worker Asimov';
-			asimovWorker = new SharedWorker('/js/worker/asimov.js');
-			workerPort = asimovWorker.port;
-		}
-		else {
-			workerName = 'Dedicated-Worker Asimov';
-			asimovWorker = new Worker('/js/worker/asimov.js');
-			workerPort = asimovWorker;
-		}
-		workerPort.onmessage = ({data}) => {
-			console.log(workerName + ' finished task-' + data.id);
-			var res = TaskPool.get(data.id);
-			TaskPool.delete(data.id);
-			if (!res) return;
-			res(data.result);
+		let workerPort, workerName;
+		let prepareWorker = () => {
+			var asimovWorker;
+			if (!!window.SharedWorker) {
+				workerName = 'Shared-Worker Asimov';
+				asimovWorker = new SharedWorker('/js/worker/asimov.js');
+				workerPort = asimovWorker.port;
+			}
+			else {
+				workerName = 'Dedicated-Worker Asimov';
+				asimovWorker = new Worker('/js/worker/asimov.js');
+				workerPort = asimovWorker;
+			}
+			workerPort.onmessage = ({data}) => {
+				console.log(workerName + ' finished task-' + data.id);
+				var res = TaskPool.get(data.id);
+				TaskPool.delete(data.id);
+				if (!res) return;
+				res(data.result);
+			};
 		};
 		let sendRequest = req => {
 			workerPort.postMessage(req);
 		};
+		prepareWorker();
 
 		window.MarkUp = window.MarkUp || {};
 		window.MarkUp.fullParse = (text, config) => new Promise(res => {
@@ -51,6 +55,10 @@ window.InitAsimov = async () => {
 			sendRequest('suicide');
 			workerPort.close();
 		};
+		PageBroadcast.on('source-updated', () => {
+			window.onbeforeunload();
+			prepareWorker();
+		});
 	}
 	// 如果不支持线程，则在浏览器主线程解析MarkUp
 	else {
